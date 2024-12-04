3d6a93ffe0cfbef0ae472341ff27ea4fb8d0cee7
duxiaoqing <duxiaoqing@pjlab.org.cn>
2024-12-04 16:16:47 +0800

    feat: 0.2.0

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -56,6 +56,64 @@ const build = async (path) => {
     console.log(`Built ${path}/dist/index.d.ts`);
 }
 
-globSync('packages/*/*').forEach(build);
+
+const list = globSync('packages/*/*');
+
+const start = async () => {
+    for (let i = 0; i < list.length; i++) {
+        const path = list[i];
+        const normalizePath = normalize(`${process.cwd()}/${path}`);
+        const file = normalize(`${normalizePath}/src/index.ts`);
+        const dist = normalize(`${normalizePath}/dist`);
+
+        if (!existsSync(file)) {
+            continue;
+        }
+
+        const esbuildConfig = {
+            entryPoints: [file],
+            external: ['@sea-lion/*'],
+            packages: 'external',
+            bundle: true,
+            sourcemap: true,
+            format: 'cjs',
+            target: 'es2022',
+            outdir: dist,
+            plugins: [lessLoader()]
+        };
+
+        await esbuild.build(esbuildConfig);
+
+        await esbuild.build({
+            ...esbuildConfig,
+            format: 'esm',
+            outExtension: { '.js': '.mjs' },
+        });
+        console.log(`Built ${path}/dist/index.mjs`);
+
+        // tsup is used to emit d.ts files only (esbuild can't do that).
+        //
+        // Notes:
+        // 1. Emitting d.ts files is super slow for whatever reason.
+        // 2. It could have fully replaced esbuild (as it uses that internally),
+        //    but at the moment its esbuild version is somewhat outdated.
+        //    It’s also harder to configure and esbuild docs are more thorough.
+        // tsup在window中的路径有问题
+        console.log(`${path}/src/index.ts`.replace(/(\/|\\|\\\\)/g, '/'));
+        await tsup.build({
+            entryPoints: [`${path}/src/index.ts`.replace(/(\/|\\|\\\\)/g, '/')],
+            format: ['cjs', 'esm'],
+            dts: { only: true },
+            outDir: dist,
+            external: [/@sea-lion\/.+/],
+            clean: true,
+        });
+        console.log(`Built ${path}/dist/index.d.ts`);
+    }
+
+}
+
+await start();
+
 
 
